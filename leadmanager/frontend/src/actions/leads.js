6aec5b053ba02ec7783d6b1c08ae8389da1a2269
yizhang61 <yzhang3250@gatech.edu>
2019-03-23 20:35:23 -0700

    Implement Rest API and React frontend

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
+import { createMessage, returnErrors } from './messages';
+
+// GET LEADS
+export const getLeads = () => dispatch => {
+    axios
+        .get("/api/leads/")
+        .then(res => {
+            dispatch({
+                type: GET_LEADS,
+                payload: res.data
+            });
+        }).catch(err => console.log(err))
+};
+
+// DELETE LEAD
+export const deleteLead = id => dispatch => {
+    axios
+        .delete("/api/leads/${id}/")
+        .then(res => {
+            dispatch(createMessage({ deleteLead: "Lead Deleted" }));
+            dispatch({
+                type: DELETE_LEAD,
+                payload: id
+            });
+        }).catch(err => console.log(err))
+};
+
+export const addLead = lead => dispatch => {
+    axios
+        .post("/api/leads/", lead)
+        .then(res => {
+            dispatch(createMessage({ addLead: "Lead Added" }));
+            dispatch({
+                type: ADD_LEAD,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(
+                returnErrors(err.response.data, err.response.status)
+            );
+        });
+};
